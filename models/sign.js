2def0bbe259b21defa7d4b40adcdade45acc5917
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-04-21 17:11:26 +0530

    first

--- models/sign.js
+++ models/sign.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const sign= new Schema({
   
 name:{type: String},
-// name:{type:String},
+name:{type:String},
 email:{type: String},
 id:{type:String},
 password:{type: Number},
